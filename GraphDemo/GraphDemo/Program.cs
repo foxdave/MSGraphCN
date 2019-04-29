7035d0009b721222341e440c4bc90fc55cd09858
Justin <foxdave@live.cn>
2019-04-29 11:10:07 +0800

    Demo for Graph 30 days CN

---
+++ GraphDemo/GraphDemo/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using ConsoleGraphTest;
+
+namespace GraphDemo
+{
+    class Program
+    {
+        //application (client) ID
+        //451912ea-1728-4fc5-b27d-453821c61aa6
+        //Directory (tenant) ID
+        //c10ade6e-bb7a-4ffc-bad2-b86a07108640
+        //client secret 
+        //u(6OCN;krp_8)+ZVs2QqOsp=qtclv-Txd10vl[;
+        //redirect URI
+        //https://localhost:8080
+
+        private static GraphServiceClient _graphServiceClient;
+        private static HttpClient _httpClient;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var config = LoadAppSettings();
+            if (null == config)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json file. Please see README.md for configuration instructions.");
+                return;
+            }
+
+            //Query using Graph SDK (preferred when possible)
+            GraphServiceClient graphClient = GetAuthenticatedGraphClient(config);
+            //在Azure AD中创建用户
+            CreateAndFindNewUser(config);
+
+            Console.WriteLine("User Created.");
+            Console.ReadKey();
+
+
+            //在.NET Core应用程序中调用Microsoft Graph获取Office 365用户信息
+            List<QueryOption> options = new List<QueryOption>
+            {
+                new QueryOption("$top", "1")
+            };
+
+            var graphResult = graphClient.Users.Request(options).GetAsync().Result;
+            Console.WriteLine("Graph SDK Result");
+            Console.WriteLine(graphResult[0].DisplayName);
+
+            //Direct query using HTTPClient (for beta endpoint calls or not available in Graph SDK)
+            HttpClient httpClient = GetAuthenticatedHTTPClient(config);
+            Uri Uri = new Uri("https://graph.microsoft.com/v1.0/users?$top=1");
+            var httpResult = httpClient.GetStringAsync(Uri).Result;
+
+            Console.WriteLine("HTTP Result");
+            Console.WriteLine(httpResult);
+
+            Console.ReadKey();
+        }
+
+        private static IConfigurationRoot LoadAppSettings()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+                // Validate required settings
+                if (string.IsNullOrEmpty(config["applicationId"]) ||
+                string.IsNullOrEmpty(config["applicationSecret"]) ||
+                string.IsNullOrEmpty(config["redirectUri"]) ||
+                string.IsNullOrEmpty(config["tenantId"]) ||
+                string.IsNullOrEmpty(config["domain"]))
+                {
+                    return null;
+                }
+
+                return config;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config)
+        {
+            var clientId = config["applicationId"];
+            var clientSecret = config["applicationSecret"];
+            var redirectUri = config["redirectUri"];
+            var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+            List<string> scopes = new List<string>();
+            scopes.Add("https://graph.microsoft.com/.default");
+
+            var cca = new ConfidentialClientApplication(clientId, authority, redirectUri, new ClientCredential(clientSecret), null, null);
+            return new MsalAuthenticationProvider(cca, scopes.ToArray());
+        }
+
+        private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
+        {
+            var authenticationProvider = CreateAuthorizationProvider(config);
+            _graphServiceClient = new GraphServiceClient(authenticationProvider);
+            return _graphServiceClient;
+        }
+
+        private static HttpClient GetAuthenticatedHTTPClient(IConfigurationRoot config)
+        {
+            var authenticationProvider = CreateAuthorizationProvider(config);
+            _httpClient = new HttpClient(new AuthHandler(authenticationProvider, new HttpClientHandler()));
+            return _httpClient;
+        }
+
+        private static void CreateAndFindNewUser(IConfigurationRoot config)
+        {
+            const string alias = "sdk_test";
+            string domain = config["domain"];
+            var userHelper = new UserHelper(_graphServiceClient);
+            userHelper.CreateUser("SDK Test User", alias, domain, "ChangeThis!0").GetAwaiter().GetResult();
+            var user = userHelper.FindByAlias(alias).Result;
+            // Console writes for demo purposes
+            Console.WriteLine(user.DisplayName);
+            Console.WriteLine(user.UserPrincipalName);
+        }
+    }
+}
